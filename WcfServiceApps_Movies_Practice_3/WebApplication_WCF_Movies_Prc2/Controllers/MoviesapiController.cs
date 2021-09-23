8063b3646cec30f57d7698358d491c936583940d
Phonginhere <tranhaiphong2016fpt@gmail.com>
2021-09-24 00:53:59 +0700

    WCF_1
    API_UPDATE

--- WcfServiceApps_Movies_Practice_3/WebApplication_WCF_Movies_Prc2/Controllers/MoviesapiController.cs
+++ WcfServiceApps_Movies_Practice_3/WebApplication_WCF_Movies_Prc2/Controllers/MoviesapiController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApplication_WCF_Movies_Prc2.Models;
 
 namespace WebApplication_WCF_Movies_Prc2.Controllers
 {
@@ -12,23 +13,31 @@ namespace WebApplication_WCF_Movies_Prc2.Controllers
     {
         public static ServiceReference1.Service1Client db = new ServiceReference1.Service1Client();
         // GET: api/Moviesapi
-        //public IEnumerable<ServiceReference1.Movies> Get()
-        //{
-        //    ServiceReference1.Service1Client db = new ServiceReference1.Service1Client();
-
-        //    return db.GetAll().AsEnumerable();
-        //}
-
-        public JToken Get()
+        public IEnumerable<ServiceReference1.Movies> Get()
         {
-            return JToken.FromObject(db.GetAll());
+            ServiceReference1.Service1Client db = new ServiceReference1.Service1Client();
+
+            return db.GetAll().AsEnumerable();
         }
+
+        //public JToken Get()
+        //{
+        //    return JToken.FromObject(db.GetAll());
+        //}
 
 
         // GET: api/Moviesapi/5
-        public string Get(int id)
+        public IHttpActionResult Get(int id)
         {
-            return "value";
+            var item = db.GetById(id);
+            Moviess m = new Moviess();
+            m.MovieId = item.MovieId;
+            m.Title = item.Title;
+            m.RunningTime = item.RunningTime;
+            m.ReleaseDate = item.ReleaseDate;
+            m.BoxOffice = item.BoxOffice;
+            m.GenreId = item.GenreId;
+            return Ok(m);
         }
 
         // POST: api/Moviesapi
@@ -38,13 +47,15 @@ namespace WebApplication_WCF_Movies_Prc2.Controllers
         }
 
         // PUT: api/Moviesapi/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, ServiceReference1.Movie value)
         {
+            db.Edit(value);
         }
 
         // DELETE: api/Moviesapi/5
         public void Delete(int id)
         {
+            db.Delete(id);
         }
     }
 }
