8b369540bdd85f2bdfbc8c96fd39a883bff73e4c
Phonginhere <tranhaiphong2016fpt@gmail.com>
2021-09-23 18:17:21 +0700

    WCF_1

---
+++ WcfServiceApps_Movies_Practice_3/WebApplication_WCF_Movies_Prc2/Models/Moviess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_WCF_Movies_Prc2.Models
+{
+    public class Moviess
+    {
+        [Key]
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public System.DateTime ReleaseDate { get; set; }
+        public int RunningTime { get; set; }
+        public int GenreId { get; set; }
+        public decimal BoxOffice { get; set; }
+
+        public virtual Genress Genress { get; set; }
+    }
+}
